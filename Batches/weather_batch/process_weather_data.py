aedf33f74257fcf9b7d9b07669e6fc2382d9b25f
Misha Kazantsev <misha.kazantsev@datatailr.com>
2023-06-28 12:33:02 +0000

    update __batch_main__ signature

--- Batches/weather_batch/process_weather_data.py
+++ Batches/weather_batch/process_weather_data.py
@@ -6,9 +6,10 @@ from rc_common import get_zeros, write_dataframe
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
 
-def __batch_main__(sub_job_name, scheduled_time, part_num, num_parts, job_config, rundate, *args):
+def __batch_main__(sub_job_name, scheduled_time, runtime, part_num, num_parts, job_config, rundate, *args):
     logging.info(f"{sub_job_name=}")
     logging.info(f"{scheduled_time=}")
+    logging.info(f"{runtime=}")
     logging.info(f"{part_num=}")
     logging.info(f"{num_parts=}")
     logging.info(f"{job_config=}")
