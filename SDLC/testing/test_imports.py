fabdf8222f8d2135c4c3c72c4e1222550ebfcc1d
Ritabrata Nag <ricky@sp4cec0wb0y.local>
2024-03-05 15:04:38 +0530

    random

---
+++ SDLC/testing/test_imports.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+try:
+    import numpy
+except:
+    import matplotlib.pyplot as plt
+
+if 5%2:
+    import selenium
+else:
+    import pytest
+
+import st_aggrid
+import sys
+import dt.smth
+
+
+if __name__ == '__main__':
+    exit(0)
